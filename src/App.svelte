b44102211be6f3b987bc91a5cf02cfad73f76709
Nurlykhan Kopenov <nurlykhan.kopenov@gmail.com>
2024-02-09 14:08:15 +0900

    initial

---
+++ src/App.svelte
@@ -0,0 +1,104 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import image1 from '/img1.gif'
+  import image2 from '/img2.gif'
+  import image3 from '/img4.gif'
+  import image4 from '/img3.gif'
+  import image5 from '/img5.gif'
+  import image6 from '/img6.gif'
+  import image7 from '/img7.gif'
+  import image8 from '/img8.gif'
+  import image9 from '/img9.gif'
+  import image10 from '/img10.gif'
+  import image11 from '/img11.gif'
+  
+
+  import Counter from './lib/Counter.svelte'
+  import Button from './lib/Button.svelte'
+ 
+
+  const images = [image1, image2, image3, image4, image5, image6,image7,image8,image9,image10, image11]
+  const labels = ['No', 'Are you sure?', 'Really sure?', 'Are you positive???', 'if you say no, I will be sad', 'I will be very very sad', 'I will be very very very sad', 'Ok fine, I will stop asking...', 'Just kidding, PLEASE SAY YES', 'PLEASEEEEEEEEE']
+  let currentImageIndex = 0;
+  let labelIndex = 0
+  let isDisabled = false
+  
+
+  // Define the initial button size and percentage change
+  const initialButtonSize = 16; // Initial font size in pixels
+  const percentageChange = 10; // Percentage change on click
+
+  let buttons = {
+    yes: {
+      label: "Yes",
+      size: initialButtonSize // Default size for Yes button
+    },
+    no: {
+      label: "No",
+      size: initialButtonSize // Default size for No button
+    }
+  };
+
+  function handleNoClick() {
+    if(labelIndex == 8){
+      isDisabled = true
+    }
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    labelIndex = (labelIndex + 1) % labels.length
+    // buttons.no.size -= (buttons.no.size * (percentageChange / 100)); // Decrease size
+    buttons.no.label = labels[labelIndex]
+    buttons.yes.size += (buttons.yes.size * (percentageChange / 100)); // Increase size of Yes button
+    buttons = {...buttons}
+    console.log(typeof buttons.no.size.toString())
+  }
+  function handleYesClick() {
+    currentImageIndex = (currentImageIndex + 10) % images.length
+    isDisabled = true
+  }
+</script>
+
+<div>
+  <div class="picture">
+    <img src={images[currentImageIndex]} class="logo" alt="Vite Logo" style="width: 200px; height: auto;" />
+  </div>
+  <h2>Will you be my Valentine?</h2>
+
+  <div class="card">
+
+    <Button text={buttons.yes.label} onClick={handleYesClick} backgroundColor="#ffe9e9" fontSize={buttons.yes.size} disabled={isDisabled}/>
+    <Button text={buttons.no.label} onClick={handleNoClick} backgroundColor="#ffe9e9" fontSize={buttons.no.size} disabled = {isDisabled}/>  
+
+  </div>
+</div>
+
+<style>
+  .picture {
+    display: flex;
+    justify-content: center;
+    height: 200px;
+  }
+  img {
+    height: 300px;
+  }
+  h2 {
+    font-size: 3rem;
+    color: #660708;
+    font-family: 'Comic Sans MS', cursive, sans-serif;
+   /* Comic Sans MS font */
+  }
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,82 @@
+<script>
+    export let text
+    export let onClick
+    export let backgroundColor
+    export let fontSize
+    export let disabled
+</script>
+
+<button disabled ={disabled} on:click={onClick} style="background-color: {backgroundColor}; font-size: {fontSize}px" class="learn-more">{text}</button>
+
+<style>
+    
+    @import url('https://fonts.googleapis.com/css?family=Rubik:700&display=swap');
+    /* $bg: #fff;
+    $text: #382b22;
+    $light-pink: #fff0f0;
+    $pink: #ffe9e9;
+    $dark-pink: #f9c4d2;
+    $pink-border: #b18597;
+    $pink-shadow: #ffe3e2; */
+    /* button {
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 16px;
+        font-weight: bold;
+    } */
+    button {
+        position: relative;
+        display: inline-block;
+        cursor: pointer;
+        outline: none;
+        border: 0;
+        vertical-align: middle;
+        text-decoration: none;
+        font-family: 'Rubik', sans-serif;
+        &.learn-more {
+            font-weight: 600;
+            color: '#fff';
+            text-transform: uppercase;
+            padding: 1.25em 2em;
+            background: light-pink;
+            border: 2px solid #b18597;
+            border-radius: 0.75em;
+            transform-style: preserve-3d;
+            transition: transform 150ms cubic-bezier(0, 0, 0.58, 1), background 150ms cubic-bezier(0, 0, 0.58, 1);
+            &::before {
+            position: absolute;
+            content: '';
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: #f9c4d2;
+            border-radius: inherit;
+            box-shadow: 0 0 0 2px #b18597, 0 0.625em 0 0 #ffe3e2;
+            transform: translate3d(0, 0.75em, -1em);
+            transition: transform 150ms cubic-bezier(0, 0, 0.58, 1), box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
+            }
+            &:hover {
+            background: #ffe9e9;
+            transform: translate(0, 0.25em);
+            &::before {
+                box-shadow: 0 0 0 2px #b18597, 0 0.5em 0 0 #ffe3e2;
+                transform: translate3d(0, 0.5em, -1em);
+            }
+            }
+            &:active {
+            background: #ffe9e9;
+            transform: translate(0em, 0.75em);
+            &::before {
+                box-shadow: 0 0 0 2px #b18597, 0 0 #ffe3e2;
+                transform: translate3d(0, 0, -1em);
+            }
+            }
+        }
+        }
+</style>
